835c0fb817de2217cb33b3f4fd5d4f0a53f76716
ali-203 <62180010+ali-203@users.noreply.github.com>
2020-11-09 23:58:22 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react"
 import './App.css';
+import Route from "./Route.js";
 
 function App() {
   return (
     <div>
-      <h1> Hello world </h1>
+      <Route />
     </div>
       
   );
--- src/Route.js
+++ src/Route.js
@@ -1,14 +1,20 @@
 import React from "react"
 import { Router } from "react-router-dom";
 import { BrowserRouter as Router, Switch, Route,} from "react-router-dom";
-
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Product from "./Components/Product";
+import ProductItem from "./Components/ProductItem";
 
 function RouteConfig() {
   return (
     <div>
       <Router> 
       <Switch>
-          <Route path="/" component={}/>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="about" component={About}/>
+          <Route exact path="product" component={Product}/>
+          <Route exact path="product/:id" component={ProductItem}/>
       </Switch>
        </Router>
     </div>
