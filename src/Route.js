ca13f568714d1ad5f67fa0910a4c5b037f314c6c
ali-203 <62180010+ali-203@users.noreply.github.com>
2020-11-09 23:05:59 +0500

    switch

---
+++ src/Route.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { Router } from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
+
+
+function RouteConfig() {
+  return (
+    <div>
+      <Router> 
+      <Switch>
+          <Route path="/" component={}/>
+      </Switch>
+       </Router>
+    </div>
+      
+  );
+  }
+export default Router;
