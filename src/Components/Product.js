923f0ef9f7dabd6bfbf23b246482ece214ff2b05
ali-203 <62180010+ali-203@users.noreply.github.com>
2020-11-11 01:13:30 +0500

    update

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -5,11 +5,11 @@ function Product() {
   return (
     <div>
       <h1> Welcome to Product </h1>
-    <div>
+    <div className="productContainer">
     {Object.keys(Shoes).map(keyName=>{
       const shoe = Shoes[keyName];
       return (<div key={keyName}>
-        <h2>{shoe.name}</h2>
+        <h4>{shoe.name}</h4>
         <img src={shoe.img} height={150}/>
       </div>)
     })}
