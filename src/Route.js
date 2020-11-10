a5a25314b5c6846ea9cfec76009383040c4d2f45
ali-203 <62180010+ali-203@users.noreply.github.com>
2020-11-10 22:40:15 +0500

    Nav bar

--- src/Route.js
+++ src/Route.js
@@ -4,16 +4,19 @@ import Home from "./Components/Home";
 import About from "./Components/About";
 import Product from "./Components/Product";
 import ProductItem from "./Components/ProductItem";
+import Navbar from "./Components/Navbar";
 
 function RouteConfig() {
 
   return (
     <div>
+    
       <Router> 
+      <Navbar />
       <Switch>
           <Route exact path="/" component={Home}/>
           <Route  path="/about" component={About}/>
-          <Route  path="/product" component={Product}/>
+          <Route exact path="/product" component={Product}/>
           <Route  path="/product/:id" component={ProductItem}/>
           <Route path="*" component={()=><h2>404 Not Found</h2>}/>
       </Switch>
