d0b20d9d00684107ffb157a003751d39b8c25822
ali-203 <62180010+ali-203@users.noreply.github.com>
2020-11-13 22:03:36 +0500

    update

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,6 +5,10 @@ function About() {
   return (
     <div>
       <h1 className="header"> Welcome to About </h1>
+      <div className="foot">
+      <p><strong> Contact us +92 3070401516 </strong>  </p>
+      <footer><strong> aliraza@gmail.com </strong>  </footer>
+      </div>
     </div>
       
   );
